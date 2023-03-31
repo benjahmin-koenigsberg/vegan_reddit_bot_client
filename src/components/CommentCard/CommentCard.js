814c63cc8783aa8890f08918c58693df29318fa1
Benjahmin Koenigsberg <magnanimousplayer@gmail.com>
2023-03-30 20:26:25 -0400

    updates to styling

--- src/components/CommentCard/CommentCard.js
+++ src/components/CommentCard/CommentCard.js
@@ -4,30 +4,30 @@ import './CommentCard.scss';
 export const CommentCard = ( {comment} ) => {
   return (
     <>
-      {/* <div className="commentCard__container"> */}
+      <div className="commentCard__container">
         <div className="comment__container">
           <div className="comment__element--title">
             <h5>{comment.link_title}</h5>
           </div>
           <div className="comment__element--subreddit">
-            <h6>{comment.subreddit}</h6>
+            <h5>{comment.subreddit}</h5>
           </div>
           <div className="comment__element--author">
-            <h6>{comment.link_author}</h6>
+            <h5>{comment.link_author}</h5>
           </div>
           <div className="comment__element--votes">
             <div className="comment__element--up">
-              <h6>{comment.ups}</h6>
+              <h5>Ups{comment.ups}</h5>
             </div>
             <div className="comment__element--down">
-              <h6>{comment.downs}</h6>
+              <h5>Downs {comment.downs}</h5>
             </div>
           </div>
           <div className="comment__element--comment">
-            <p>{comment.body}</p>
+            <h6>{comment.body}</h6>
           </div>
         </div>
-      {/* </div> */}
+      </div>
     </>
   );
 }
