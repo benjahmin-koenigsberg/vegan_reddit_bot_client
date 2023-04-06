412da18ef5849e133edd24da89c357038b6bea3e
Benjahmin Koenigsberg <magnanimousplayer@gmail.com>
2023-04-06 17:46:35 -0400

    updates to new axios return for sub stream

--- src/components/CommentCard/CommentCard.js
+++ src/components/CommentCard/CommentCard.js
@@ -9,21 +9,21 @@ import downsIcon from "../../assets/downs.jpeg";
 export const CommentCard = ( {post} ) => {
   return (
     // <Link to={comment.permalink}>
-      <div className="commentCard__container">
-        <div className="comment__container">
-          <div className="comment__container--labels">post Title</div>
-          <div className="comment__element--title">
-            <h5>{post.title}</h5>
-          </div>
-          <div className="comment__container--labels">r/subreddit</div>
-          <div className="comment__element--subreddit">
-            <h5>{post.subreddit}</h5>
-          </div>
-          {/* <div className="comment__container--labels">post author</div>
+    <div className="commentCard__container">
+      <div className="comment__container">
+        <div className="comment__container--labels">post Title</div>
+        <div className="comment__element--title">
+          <h5>{post.header_title}</h5>
+        </div>
+        <div className="comment__container--labels">r/subreddit</div>
+        <div className="comment__element--subreddit">
+          <h5>{post.display_name_prefixed}</h5>
+        </div>
+        {/* <div className="comment__container--labels">post author</div>
           <div className="comment__element--author">
             <h5>{post.author}</h5>
           </div> */}
-          {/* <div className="comment__container--labels">votes</div>
+        {/* <div className="comment__container--labels">votes</div>
           <div className="comment__element--votes">
             <img src={upsIcon} className="comment__elemnet--icons-up"></img>
 
@@ -37,12 +37,12 @@ export const CommentCard = ( {post} ) => {
               <h5>{comment.downs}</h5>
             </div>
           </div> */}
-          <div className="comment__container--labels">post content</div>
-          <div className="comment__element--comment">
-            <h6 >{post?.selftext}</h6>
-          </div>
+        <div className="comment__container--labels">post content</div>
+        <div className="comment__element--comment">
+          <h6>{post?.submit_text}</h6>
         </div>
       </div>
+    </div>
     // </Link>
   );
 }
